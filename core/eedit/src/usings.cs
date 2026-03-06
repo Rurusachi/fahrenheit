0ff957982f3316a81fea95f47c4e21521d5eebea
Fran Kelava <fran.kelava@outlook.com>
2026-03-06 14:24:57 +0100

    Merge EEdit into mainline.
    Commit history (8 commits) not subtree-merged as was deemed
    unnecessary, can be inspected at ``fh-tools-eedit``.

---
+++ core/eedit/src/usings.cs
@@ -0,0 +1,21 @@
+﻿// C# stdlib essentials
+global using System;
+global using System.Runtime.InteropServices;
+global using System.Runtime.Versioning;
+global using System.Numerics;
+global using System.Diagnostics.CodeAnalysis;
+global using System.IO;
+global using System.Text;
+
+// Native file dialog bindings
+global using NativeFileDialogCore;
+
+// ImGui C# binding
+global using Hexa.NET.ImGui;
+
+// Fahrenheit support libraries
+global using Fahrenheit.FFX;
+
+// Win32/D3D11 typedefs
+global using TerraFX.Interop.DirectX;
+global using TerraFX.Interop.Windows;
